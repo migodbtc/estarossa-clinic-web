cac197f64c78635c00162933c6359de92b12cb43
migodbtc <mjdmbunda@dbtcmandaluyong.one-bosco.org>
2025-12-19 21:58:25 +0800

    Added better background for the auth page layout

--- web/src/app/(web)/(auth)/layout.tsx
+++ web/src/app/(web)/(auth)/layout.tsx
@@ -4,10 +4,12 @@ import React, { PropsWithChildren } from "react";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-200 text-slate-800">
-      <div className="mx-auto w-[30vw]">
-        <div className="bg-white px-8 py-12 rounded-xl w-full mx-auto">
-          {children}
+    <div className="w-screen h-screen flex items-center justify-center bg-auth-layout">
+      <div className="w-full h-full flex items-center justify-center bg-slate-200/25 backdrop-blur-lg text-slate-800">
+        <div className="mx-auto w-[30vw]">
+          <div className="bg-white px-8 py-12 rounded-xl w-full mx-auto">
+            {children}
+          </div>
         </div>
       </div>
     </div>
