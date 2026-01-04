82d3dd9b15f231462fc420a97c6ac348d22e5a69
migodbtc <mjdmbunda@dbtcmandaluyong.one-bosco.org>
2026-01-04 15:56:27 +0800

    Added code for login specifically for retrieving payload, sending request to Flask API, and handling the 401 status code

--- web/src/app/api/auth/login/route.ts
+++ web/src/app/api/auth/login/route.ts
@@ -1,6 +1,47 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-  } catch (error: any) {}
+    // mandatory constants
+    const data = await request.json();
+    const api = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const body = JSON.stringify(data);
+
+    // do request to auth login route in flask api
+    const response = await fetch(api + "/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body,
+    });
+    const result = await response.json();
+    const status = await response.status;
+
+    console.log("Result of the login request:");
+    console.log(result);
+    console.log(`Status Code: ${status}`);
+
+    // error handling: errors come in this payload
+    // { error: 'invalid credentials' }
+
+    // invalid credentials (code 401)
+    if (status == 401) {
+      console.error("Login form is provided with invalid credentials!");
+      return NextResponse.json(
+        {
+          status: "error",
+          message: "Invalid login! Please check email address or password.",
+        },
+        {
+          status: response.status,
+        }
+      );
+    }
+  } catch (err: any) {
+    // generic catch statement
+    console.error("Registration error: ", err);
+    return NextResponse.json(
+      { status: "error", message: "Registration failed!" },
+      { status: 400 }
+    );
+  }
 }
