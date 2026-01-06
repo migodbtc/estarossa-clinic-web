a014dac3439ff149f15f727bf1f6390e4af8199a
migodbtc <mjdmbunda@dbtcmandaluyong.one-bosco.org>
2026-01-06 15:59:11 +0800

    Added validation check for token from cookie storage to middleware/proxy file. Additionally, skipping validation for /login and /register for now until finished with the priority routes

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -1,18 +1,45 @@
+import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
 
 // middleware config
 export const config = {
   // ssr routes
-  matcher: ["/api/:path*", "/workspace/:path*"],
+  matcher: ["/api/:path*", "/workspace/:path*", "/login", "/register"],
 };
 
 // main middleware
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   console.log("Proxy triggered!");
 
-  const token = request.headers.get("authorization");
+  const token = request.cookies.get("refresh_token_cookie")?.value;
 
-  console.log(token);
+  // CSR routes: skip auth check for now
+  const { pathname } = request.nextUrl;
+  if (pathname === "/login" || pathname === "/register") {
+    return NextResponse.next();
+  }
+
+  // redirect to login if there is no user
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // additional validation if token = true
+  try {
+    const { payload } = await jwtVerify(
+      token,
+      new TextEncoder().encode("dev-secret-change-me")
+    );
+
+    const exp = payload.exp;
+
+    // date validation
+    if (exp && exp <= Math.floor(Date.now() / 1000)) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  } catch (e) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   return NextResponse.next();
 }
